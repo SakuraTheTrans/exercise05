d6e1af8d0d4bcc3b4e8dde7a82faaf7115f4e850
Sakura <sakurathetrans@gmail.com>
2025-02-11 14:34:52 -0500

    all changes !

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class AddressBook {
+    static ArrayList<Person> contactList = new ArrayList<>();
+
+    public AddressBook() {
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contactList=" + contactList +
+                '}';
+    }
+
+    // Methods
+
+    void getContact (String a) {
+        for (Person person : contactList) {
+            if (a.equals(person.getName())) {
+                System.out.println(person.getEmail());
+            }
+        }
+    }
+
+    void addContact (Person a) {
+        contactList.add(a);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in);
+
+        AddressBook blackBook = new AddressBook();
+
+        while(true){
+
+            System.out.println("Would you like to add a person to the blackbook? (Y/N)");
+            String userIn = input.nextLine();
+
+            if (userIn.equalsIgnoreCase("y")){
+
+                System.out.print("Please input a name : ");
+                String name = input.nextLine();
+                System.out.print("Please input a email : ");
+                String email = input.nextLine();
+                Person placeholder = new Person(name, email);
+                blackBook.addContact(placeholder);
+
+            } else {
+
+                System.out.println("Your blackbook contains : " + blackBook);
+
+                while(true){
+
+                    System.out.println("Would you like to search for a person in the blackbook? (Y/N)");
+                    userIn = input.nextLine();
+
+                    if (userIn.equalsIgnoreCase("y")){
+
+                        System.out.print("Persons Name : ");
+                        String userLookup = input.nextLine();
+                        blackBook.getContact(userLookup);
+
+                    } else {
+
+                        break;
+
+                    }
+                }
+
+                break;
+
+            }
+        }
     }
 }
